b904810a2466fc08f3451fb18af853eb56b0725e
Jérôme Avoustin <jerome.avoustin@sundayapp.com>
2024-06-05 00:46:13 +0200

    fix tests for step-4 vos

--- src/test/java/com/sunday/KitchenTest.java
+++ src/test/java/com/sunday/KitchenTest.java
@@ -8,14 +8,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class KitchenTest {
+    OrderableProduct ipa = new OrderableProduct("IPA 25cl", 500);
+    OrderableProduct belgianBeer = new OrderableProduct("Belgian Beer 33cl", 600);
+    OrderableProduct chips = new OrderableProduct("Chips", 200);
+
     @Test
     @Disabled
     void should_list_order_in_preparation() {
         // Given
         var kitchen = new Kitchen();
         var order1 = new Order("O-1234", "On est assez pressés! Merci de faire vite!");
+        order1.addProduct(ipa, 2);
+        order1.addProduct(chips, 1);
         var order2 = new Order("O-1235");
+        order2.addProduct(belgianBeer, 1);
         var order3 = new Order("O-1236", "Je crains la coriandre");
+        order3.addProduct(ipa, 1);
+        order3.addProduct(belgianBeer, 1);
+
         kitchen.receiveOrder(order1);
         kitchen.receiveOrder(order2);
         kitchen.receiveOrder(order3);
